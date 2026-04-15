af86241671f69236877e857e8d1a6f774a6cbf1c
Malak <malakalsinani@gmail.com>
2026-04-15 20:56:57 +0400

    Add Instructors JS File(First Update)

---
+++ js/instructors.js
@@ -0,0 +1,7 @@
+const instructorsContainer = document.getElementById("instructorsContainer");
+const instructorSearch = document.getElementById("instructorSearch");
+const countBadge = document.getElementById("count");
+const toggleBtn = document.getElementById("toggle_btn");
+
+let allInstructors = [];
+
