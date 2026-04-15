19977c07d8fb1631d006d3df7aa2e68be844e9df
Malakaka <malakalsenine@gmail.com>
2026-04-15 13:40:26 +0400

    update

---
+++ js/course-details.js
@@ -0,0 +1,7 @@
+const courseDetails = document.getElementById("courseDetails");
+const quizForm = document.getElementById("quizForm");
+const submitQuizBtn = document.getElementById("submitQuiz");
+const quizResult = document.getElementById("quizResult");
+const countBadge = document.getElementById("count");
+const toggleBtn = document.getElementById("toggle_btn");
+
