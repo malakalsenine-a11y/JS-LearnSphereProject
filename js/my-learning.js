7c6c938b3ce9b0036bee99faadac64b3d3f95aff
Malak <malakalsinani@gmail.com>
2026-04-15 21:05:39 +0400

    Add My Learning JS File (Load enrolled courses)

--- js/my-learning.js
+++ js/my-learning.js
@@ -3,3 +3,10 @@ const statsContainer = document.getElementById("statsContainer");
 const countBadge = document.getElementById("count");
 const toggleBtn = document.getElementById("toggle_btn");
 
+// ===== Load enrolled courses =====
+let enrolledCourses = JSON.parse(localStorage.getItem("enrolled")) || [];
+
+displayMyCourses();
+updateNavbarCount();
+applySavedTheme();
+
