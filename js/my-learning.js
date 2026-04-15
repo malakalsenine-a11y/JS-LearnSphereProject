0a98c3cc6df0501d76b771fef23f968716a4e61d
Malak <malakalsinani@gmail.com>
2026-04-15 21:04:14 +0400

    Add My Learning JS File (First update)

---
+++ js/my-learning.js
@@ -0,0 +1,5 @@
+const myCoursesContainer = document.getElementById("myCoursesContainer");
+const statsContainer = document.getElementById("statsContainer");
+const countBadge = document.getElementById("count");
+const toggleBtn = document.getElementById("toggle_btn");
+
